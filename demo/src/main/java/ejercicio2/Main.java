d585678d62c9081b7f5d945934565cf5bb421fde
bbotelho42 <bbotelho@student.42barcelona.com>
2024-10-23 12:55:34 +0200

    automatic save home

--- demo/src/main/java/ejercicio2/Main.java
+++ demo/src/main/java/ejercicio2/Main.java
@@ -2,6 +2,18 @@ package ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        // EJERCICIO 2
+        int[] array = new int[5];
+        int value = 10;
+        for (int i = 0; i < array.length; i++){
+            array[i] = value;
+            value += 10;
+        }
+        for (int i = 0; i < array.length; i++){
+            System.out.print("["+array[i]+"]");
+        }
+        System.out.println();
+
     }
 }
