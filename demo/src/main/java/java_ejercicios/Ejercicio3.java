b8e5fbe7a15e0ff1f3f619e165fc930b40639063
bbotelho42 <bbotelho@student.42barcelona.com>
2024-10-24 12:17:41 +0200

    automatic save home

--- demo/src/main/java/java_ejercicios/Ejercicio3.java
+++ demo/src/main/java/java_ejercicios/Ejercicio3.java
@@ -21,9 +21,9 @@ public class Ejercicio3 {
   // STACKS PILAS
   public static void myStack(){
     Stack<Integer> newStack = new Stack<>();
-    newStack.add(15);
-    newStack.add(56);
-    newStack.add(65);
+    newStack.push(15);
+    newStack.push(56);
+    newStack.push(65);
     System.out.println("Numeros insertados: "+newStack+"\nNumero removido: "+newStack.pop()+"\nNumero en el topo de la pila: "+newStack.peek());
   }
   
