1321966eba4ef081441fc3aa43227ba9ec6d0ca3
bbotelho42 <brunnowrdev@gmail.com>
2025-07-19 08:02:07 +0200

    ex_2 with java

---
+++ demo/src/main/java/java_ejercicios/java_CFGS_DAM/ex_02/ex2/School.java
@@ -0,0 +1,9 @@
+
+package  java_ejercicios.java_CFGS_DAM.ex_02.ex2;
+
+public class School{
+
+  public static void main(String[] args){
+      System.out.println("##########___EscuelaX___##########");
+  }
+}
